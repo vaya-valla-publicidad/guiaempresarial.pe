ce3a90aece6d178db62ffcf698e3098985e1f22d
Abelperez20 <abelperezcoca058@gmail.com>
2026-02-19 18:14:31 -0500

    Elaboración de login y mejora en el diseño

---
+++ parte01/includes/Header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+       <nav>
+            <a href="index.php">Inicio</a> |
+            <a href="empresas.php">Empresas</a> |
+            <a href="categorias.php">Categorías</a> |
+            <a href="contacto.php">Contacto</a> |
+            <a href="sobre.php">Sobre Nosotros</a>
+       </nav>
+    </header>
+    <main>
